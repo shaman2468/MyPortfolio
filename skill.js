b553e15877ee2c1edc029b48e6ef0aba3c96384e
Portfolio-Rojo <jademarkrojo2020@gmail.com>
2025-12-08 17:15:48 +0800

    last change

--- skill.js
+++ skill.js
@@ -25,3 +25,19 @@ document.getElementById("close-menu-btn").addEventListener("click", () => {
     document.getElementById("mobile-nav").classList.remove("open");
 });
 
+function changeText() {
+    const changingTextElement = document.getElementById("changingText");
+    
+    // Add fade-out effect before changing text
+    changingTextElement.classList.add("fade-text");
+
+    setTimeout(() => {
+        // Change the text
+        const baseText = "IT Student & ";
+        changingTextElement.textContent = baseText + textArray[index];
+        index = (index + 1) % textArray.length; // Loop back to the first item when we reach the end
+        
+        // Remove fade effect after text change and trigger fade-in
+        changingTextElement.classList.remove("fade-text");
+    }, 500); // Wait 0.5s before changing text to allow the fade-out effect
+}
